6e5ef968a7957fd8cc9871a5a2080eb890bae78b
Clément <clement2104.boillot@gmail.com>
2024-01-26 16:41:26 +0100

    Update script

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+cflags="-W -Wall -Wextra -g3 -fsanitize=leak,undefined,address"
+cc="g++ -std=c++20"
 
 for i in {0..3}; do
-    g++ -W -Wall -Wextra -g3 -fsanitize=leak,undefined,address *.cpp "bonus/ex0$i.cpp"
-    if  ./a.out | diff "bonus/out0$i.txt" - ; then
-        echo "Test $i: OK"
-    else
-        echo "Test $i: KO"
-    fi
+    $cc $cflags *.cpp "bonus/ex0$i.cpp" || exit $?
+    ./a.out | diff "bonus/out0$i.txt" - || exit $?
+    echo -e "Test $i: \033[1;32mOK\033[00m"
 done
